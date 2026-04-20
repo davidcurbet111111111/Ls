14defc4cc6285e9637906114e63826f5aa3d23cb
davidcurbet111111111 <davidcurbet111111111@protonmail.com>
2026-04-20 12:30:20 +0300

    Create win10.sh

---
+++ win10.sh
@@ -0,0 +1,20 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+export HOME="/data/data/com.termux/files/home"
+cd ~/base_arm64/khanh 2> /dev/null
+echo "VNC Server: 127.0.0.1:5901"
+
+qemu-system-aarch64 \
+  -M virt \
+  -cpu cortex-a53 \
+  -smp 4 \
+  --accel tcg,thread=multi \
+  -m 1024 \
+  -bios BIOS.img \
+  -device VGA \
+  -device nec-usb-xhci \
+  -device usb-kbd \
+  -device usb-mouse \
+  -device usb-storage,drive=boot \
+  -drive if=none,id=boot,file="base_arm64.qcow2" \
+  -vnc :1
